ed54c1978319d4554221d9fe085101d8018e2c26
Vladislav Gusev <gysevvlad@gmail.com>
2021-10-22 07:35:32 +0300

    second attempt, looks ok

---
+++ cmake/x64-linux-clang-12-libcxx-ldd.toolchain.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_CXX_COMPILER_INIT "/usr/bin/clang++-12")
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld -rtlib=compiler-rt -lunwind")
---
+++ cmake/x64-linux-clang-libcxx.cmake
@@ -0,0 +1,8 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+#set(VCPKG_LINKER_FLAGS "-fuse-ld=lld -rtlib=compiler-rt -lunwind")
+#set(VCPKG_CXX_FLAGS "-stdlib=compiler-rt")
+#set(VCPKG_C_FLAGS "")
